1f108276855a9459bdd6a80f275485c2c5397afc
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-06-01 01:16:02 +0200

    remove groth16 setups

--- backend/accelerated/webgpu/groth16/groth16.go
+++ backend/accelerated/webgpu/groth16/groth16.go
@@ -8,9 +8,6 @@ import (
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/backend/groth16"
-	groth16_bls12377 "github.com/consensys/gnark/backend/groth16/bls12-377"
-	groth16_bls12381 "github.com/consensys/gnark/backend/groth16/bls12-381"
-	groth16_bn254 "github.com/consensys/gnark/backend/groth16/bn254"
 	"github.com/consensys/gnark/backend/witness"
 	"github.com/consensys/gnark/constraint"
 	csbls12377 "github.com/consensys/gnark/constraint/bls12-377"
@@ -48,63 +45,6 @@ func Prove(r1cs constraint.ConstraintSystem, pk groth16.ProvingKey, fullWitness 
 			return nil, fmt.Errorf("webgpu groth16: expected *webgpu_bls12381.ProvingKey, got %T", pk)
 		}
 		return webgpu_bls12381.Prove(_r1cs, _tpk, fullWitness, opts...)
-	default:
-		return nil, fmt.Errorf("webgpu groth16: unsupported constraint system %T", r1cs)
-	}
-}
-
-// Setup wraps gnark's Groth16 setup and returns a proving key wrapper that can
-// later pin packed MSM bases in the browser runtime.
-func Setup(r1cs constraint.ConstraintSystem) (groth16.ProvingKey, groth16.VerifyingKey, error) {
-	switch _r1cs := r1cs.(type) {
-	case *csbn254.R1CS:
-		var pk webgpu_bn254.ProvingKey
-		var vk groth16_bn254.VerifyingKey
-		if err := groth16_bn254.Setup(_r1cs, &pk.ProvingKey, &vk); err != nil {
-			return nil, nil, err
-		}
-		return &pk, &vk, nil
-	case *csbls12377.R1CS:
-		var pk webgpu_bls12377.ProvingKey
-		var vk groth16_bls12377.VerifyingKey
-		if err := groth16_bls12377.Setup(_r1cs, &pk.ProvingKey, &vk); err != nil {
-			return nil, nil, err
-		}
-		return &pk, &vk, nil
-	case *csbls12381.R1CS:
-		var pk webgpu_bls12381.ProvingKey
-		var vk groth16_bls12381.VerifyingKey
-		if err := groth16_bls12381.Setup(_r1cs, &pk.ProvingKey, &vk); err != nil {
-			return nil, nil, err
-		}
-		return &pk, &vk, nil
-	default:
-		return nil, nil, fmt.Errorf("webgpu groth16: unsupported constraint system %T", r1cs)
-	}
-}
-
-// DummySetup mirrors gnark's DummySetup and returns a proving key wrapper for
-// deserialization and testing.
-func DummySetup(r1cs constraint.ConstraintSystem) (groth16.ProvingKey, error) {
-	switch _r1cs := r1cs.(type) {
-	case *csbn254.R1CS:
-		var pk webgpu_bn254.ProvingKey
-		if err := groth16_bn254.DummySetup(_r1cs, &pk.ProvingKey); err != nil {
-			return nil, err
-		}
-		return &pk, nil
-	case *csbls12377.R1CS:
-		var pk webgpu_bls12377.ProvingKey
-		if err := groth16_bls12377.DummySetup(_r1cs, &pk.ProvingKey); err != nil {
-			return nil, err
-		}
-		return &pk, nil
-	case *csbls12381.R1CS:
-		var pk webgpu_bls12381.ProvingKey
-		if err := groth16_bls12381.DummySetup(_r1cs, &pk.ProvingKey); err != nil {
-			return nil, err
-		}
-		return &pk, nil
 	default:
 		return nil, fmt.Errorf("webgpu groth16: unsupported constraint system %T", r1cs)
 	}
